eb50dc016749b3b16d725ee6499fffc98e9750a0
unknown <romanidel15@gmail.com>
2022-04-10 13:15:18 +0300

    Ваш текст

---
+++ 4.Lesson/Untitled-1.py
@@ -0,0 +1,26 @@
+print('Hello World')
+
+first = 23
+second = "Yellow"
+print(first,second)
+
+
+a = 10 + 5
+b = a + 30
+c = (a+b) * 2
+
+print(a,b,c)
+
+
+first_string = "Hello World!"
+second_string = 'Hello World!'
+third_string = "'Hello World'"
+
+name = input("Ваше Ім'я?")
+
+greeting = "Привіт," + name
+print(greeting)
+
+text = input("Напишіть любий текст:")
+text_lenght = len(text)
+print(text_lenght)
